03efb18ad3653363a67b2a56981f6f665263a166
hashaam-1 <hashaamamz1@gmail.com>
2026-04-14 02:32:53 +0500

    db

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI not found in environment variables");
+    throw new Error("MONGODB_URI is missing in environment variables");
   }
 
-  return mongoose.connect(process.env.MONGODB_URI);
+  const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    serverSelectionTimeoutMS: 10000,
+  });
+
+  console.log("MongoDB Connected:", conn.connection.host);
+
+  return conn;
 };
 
 module.exports = connectDB;
