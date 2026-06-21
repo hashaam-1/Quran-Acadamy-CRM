12b8fb8c789b2ff6c81f2d54d15a0c0b999c7d93
hashaam-1 <hashaamamz1@gmail.com>
2026-06-21 23:01:59 +0500

    Fix CORS: Add cache-control and other headers to allowedHeaders

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
+  allowedHeaders: ['Content-Type','Authorization','cache-control','X-Requested-With','Accept']
 }));
 app.options('*', cors());
 app.use(helmet());
