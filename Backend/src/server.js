83f7dade62597f8362db065314b27476a5e39d07
hashaam-1 <hashaamamz1@gmail.com>
2026-06-21 23:17:26 +0500

    server updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization','cache-control','X-Requested-With','Accept']
+  allowedHeaders: ['Content-Type','Authorization','cache-control','X-Requested-With','Accept','pragma']
 }));
 app.options('*', cors());
 app.use(helmet());
