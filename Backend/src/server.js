262d0d16b832da312969a8d8717ed9cce06d4ff4
hashaam-1 <hashaamamz1@gmail.com>
2026-06-21 23:36:49 +0500

    Fix CORS: Add 'expires' header to allowedHeaders

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization','cache-control','X-Requested-With','Accept','pragma']
+  allowedHeaders: ['Content-Type','Authorization','cache-control','X-Requested-With','Accept','pragma','expires']
 }));
 app.options('*', cors());
 app.use(helmet());
